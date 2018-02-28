342a467536f7d12f5e265ed892d5428351633a8f
Holly <holly.wilkalis@gmail.com>
2018-02-28 13:50:10 -0800

    Add draft of state tree

---
+++ planning docs/state-tree.jsx
@@ -0,0 +1,85 @@
+{
+  imagesById: {
+    1: {
+      imgTitle: 'Clinic shot with certifier and med tech',
+      longDesc: 'Clinic setting with young participant and medical technician and WIC coordinator'
+      contentCategory: 'clinic',
+      contentKeywords: [
+        'clinic',
+        'medical',
+        'technician',
+        'child',
+        'toddler',
+        'participant'
+      ],
+      orientation: 'landscape',
+      thumbnailURL: 'https://github.com/hollywilkalis/capstone-planning/blob/master/samples/CLN_014_PPT.jpg',
+      socialmediaURL: 'https://github.com/hollywilkalis/capstone-planning/blob/master/samples/CLN_014_FB_cover.jpg',
+      printURL: 'https://github.com/hollywilkalis/capstone-planning/blob/master/samples/CLN_014_print.jpg',
+      powerPointURL: 'https://github.com/hollywilkalis/capstone-planning/blob/master/samples/CLN_014_PPT.jpg',
+      imageStyle: 'photo',
+      location: 'Douglas County, Oregon'
+      photoDate: '09/2018',
+      photographerId: '',
+      useRestrictions: 'for Oregon WIC and WIC clinic use only',
+      },
+
+    2: {
+      imgTitle: 'Participant checking in at clinic',
+      longDesc: 'Shot of a participant and child checking in for WIC appointment'
+      contentCategory: 'clinic',
+      contentKeywords: [
+        'clinic',
+        'participant',
+        'toddler',
+        'woman',
+        'check-in',
+        'administration',
+        'front desk'
+      ],
+      orientation: 'landscape',
+      thumbnailURL: 'https://github.com/hollywilkalis/capstone-planning/blob/master/samples/CLN_025_FB.jpg',
+      socialmediaURL: 'https://github.com/hollywilkalis/capstone-planning/blob/master/samples/CLN_025_FB_cover.jpg',
+      printURL: 'https://github.com/hollywilkalis/capstone-planning/blob/master/samples/CLN_025_print.jpg',
+      powerPointURL: 'https://github.com/hollywilkalis/capstone-planning/blob/master/samples/CLN_025_FB.jpg',
+      imageStyle: 'photo',
+      location: 'Josephine County, Oregon'
+      photoDate: '09/2017',
+      photographerId: 'p2',
+      useRestrictions: 'for Oregon WIC and WIC clinic use only',
+      },
+
+      3: {
+        imgTitle: 'Breastfeeding buddies',
+        longDesc: 'Overhead shot of two mothers sitting on a blanket at a park while breastfeeding their babies',
+        contentCategory: 'breastfeeding',
+        contentKeywords: [
+          'breastfeeding',
+          'woman',
+          'baby',
+          'outdoors',
+          'park'
+        ],
+        orientation: 'landscape',
+        thumbnailURL: 'https://github.com/hollywilkalis/capstone-planning/blob/master/samples/BFMB_035_PPT.jpg',
+        socialmediaURL: 'https://github.com/hollywilkalis/capstone-planning/blob/master/samples/BFMB_035_FB_cover.jpg',
+        printURL: 'https://github.com/hollywilkalis/capstone-planning/blob/master/samples/BFMB_035_print.jpg',
+        powerPointURL: 'https://github.com/hollywilkalis/capstone-planning/blob/master/samples/BFMB_035_PPT.jpg',
+        imageStyle: 'photo',
+        location: 'Washington County, Oregon'
+        photoDate: '09/2017',
+        photographerId: 'p1',
+        useRestrictions: 'for Oregon WIC and WIC clinic use only',
+        }
+      },
+    photographersById: {
+      p1: {
+        photographerName: 'Sunny Mancuso',
+        photographerURL: 'http://www.google.com'
+      },
+      p2: {
+        photographerName: 'Mark Romine'
+        photographerURL: 'http://www.google.com'
+      }
+    }
+  }
